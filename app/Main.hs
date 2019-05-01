4609dba415ab944b517e41cabc2af920a025ebec
Marten Agren <marangisto@me.com>
2019-05-01 15:02:51 +0800

    Somewhat working code generation.

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DeriveDataTypeable, RecordWildCards, OverloadedStrings #-}
 module Main where
 
 import Graphics.Rendering.TrueType.STB
@@ -5,28 +6,116 @@ import Data.Array.Unboxed (UArray)
 import Control.Monad
 import Data.Array.IArray
 import Data.Word
+import Data.Char (ord)
+import Data.List (intercalate)
+import Numeric (showHex)
+import System.Console.CmdArgs
+
+data Options = Options
+    { size    :: Float
+    , files   :: [FilePath]
+    } deriving (Show, Eq, Data, Typeable)
+
+options :: Main.Options
+options = Main.Options
+    { size = def &= help "bitmap height"
+    , files = def &= args &= typ "FILES"
+    } &=
+    verbosity &=
+    help "Rasterize TrueType fonts for embedded use" &=
+    summary "Fonterizer v0.0.0, (c) Bengt Marten Agren 2019" &=
+    details [ "Fonterizer converts a TrueType font to C++ bitmap definitions."
+            ]
 
 main :: IO ()
 main = do
-    tt <- loadTTF "ttf/SourceCodeVariable-Roman.ttf"
+    opts@Options{..} <- cmdArgs $ options { size = 20 }
+    mapM_ putStrLn preamble
+    mapM_ (process size) files
+
+data GlyphData = GlyphData
+    { char      :: Char
+    , width     :: Int
+    , height    :: Int
+    , offsetH   :: Int
+    , offsetV   :: Int
+    , bitmap    :: UArray (Int, Int) Word8
+    }
+
+preamble :: [String]
+preamble =
+    [ "#include <stdint.h>"
+    , ""
+    , "struct glyph_t"
+    , "{"
+    , "    uint8_t      width;"
+    , "    uint8_t      height;"
+    , "    int8_t       offset_h;"
+    , "    int8_t       offset_v;"
+    , "    const uint8_t *bitmap;"
+    , "};"
+    , ""
+    ]
+
+process :: Float -> FilePath -> IO ()
+process size file = do
+    tt <- loadTTF file
     (i:_) <- enumerateFonts tt
-    ft <- initFont tt i
-    forM_ [' '..'~'] $ \c -> do
-        Just gy <- findGlyph ft c
-        vmu <- getFontVerticalMetrics ft
-        let s = scaleForPixelHeight vmu 60
-        (bm, bo) <- newGlyphBitmap ft gy (s, s)
-        putStrLn $ c : ": " <> show (bitmapSize bm) <> " " <> show bo
-        showBitMap (bitmapSize bm) =<< bitmapArray bm
-    putStrLn "done!"
+    font <- initFont tt i
+    let cs = ['A'..'C']
+    xs <- mapM (glyphData font size) cs
+    mapM_ (putStrLn . unlines . bitmapDecl) xs
+    putStrLn $ "extern glyph_t font[" <> show (length cs) <> "] ="
+    mapM_ putStrLn $ map indent $ initializer $ map glyphDecl xs
+
+glyphData :: Font -> Float -> Char -> IO GlyphData
+glyphData font size char = do
+    Just glyph <- findGlyph font char
+    vmu <- getFontVerticalMetrics font
+    let s = scaleForPixelHeight vmu size
+    (bitmap', (offsetH, offsetV)) <- newGlyphBitmap font glyph (s, s)
+    let (width, height) = bitmapSize bitmap'
+    bitmap <- bitmapArray bitmap'
+    return GlyphData{..}
+
+glyphDecl :: GlyphData -> String
+glyphDecl GlyphData{..} = "{ " <> intercalate ", "
+    [ show width
+    , show height
+    , show offsetH
+    , show offsetV
+    , bitmapName char
+    ] <> " }" <> " // '" <> [ char ] <> "'"
+
+bitmapDecl :: GlyphData -> [String]
+bitmapDecl GlyphData{..}  = 
+    [ "// '" <> [ char ] <> "'"
+    , "static uint8_t " <> bitmapName char <> "[] ="
+    ] ++ (map indent $ initializer $ map row [0..height-1])
+    where row r = intercalate ", " $ map (col r) [0..width-1]
+          col r c = hex $ bitmap!(r, c)
+
+bitmapName :: Char -> String
+bitmapName char = "bitmap_" <> (show $ ord char)
 
 showBitMap :: (Int, Int) -> UArray (Int, Int) Word8 -> IO ()
 showBitMap (w, h) bs = do
     forM_ [0..h-1] $ \r -> do
+        putStr "//    "
         forM_ [0..w-1] $ \c -> do
             putStr $ grayLevel $ bs!(r, c)
         putStrLn ""
 
 grayLevel :: Word8 -> String
 grayLevel x = show $ x `div` 32
+
+hex :: Word8 -> String
+hex x = "0x" ++ showHex x ""
+
+initializer :: [String] -> [String]
+initializer [] = [ "{", "};" ]
+initializer (x:xs) = "{ " <> x : map (", "<>) xs ++ [ "};" ]
+
+indent :: String -> String
+indent = ("    "<>)
 
